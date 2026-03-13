efc460952507d295d7478b8b970afe52b2c62758
IgYaHiko <subhrokolay2@gmail.com>
2026-03-14 00:21:06 +0530

    [SYNTAX]-[06]:omggggggg-stop-this-crap

---
+++ fizz-buzz.py
@@ -0,0 +1,11 @@
+def fizz_buzz():
+
+    for i in range(1, 100):
+        if(i % 3 == 0):
+            print("fizz"); 
+        elif (i % 5 == 0):
+            print("buzz");
+        else:
+            print(i)
+        
+fizz_buzz()
