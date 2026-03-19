477d91b7e21a02a58bf17e2ace209040a59f2192
IgYaHiko <subhrokolay2@gmail.com>
2026-03-19 10:45:44 +0530

    [String]-[18]:jhuhsuhcushw

---
+++ find-len.py
@@ -0,0 +1,11 @@
+def find_len(sen):
+    sen = input("Enter Your String: ")
+
+    count = 0;
+    for i in sen:
+        count += 1
+    print(f"Len of the string: {count}")
+    return count
+
+
+find_len("Subhro")
