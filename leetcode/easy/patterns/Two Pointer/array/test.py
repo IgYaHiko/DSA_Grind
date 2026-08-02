0dbb91d385e4406baf0fa8053d4bba5d493cf9be
IgYaHiko <subhrokolay2@gmail.com>
2026-08-02 10:54:41 +0530

    71. Simplify Path

--- leetcode/easy/patterns/Two Pointer/array/test.py
+++ leetcode/easy/patterns/Two Pointer/array/test.py
@@ -1,4 +1,24 @@
-n = [1,3,5,6,76,6]
-stack = []
-stack.append(len(n)-1)
-print(stack)
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        res = "/"
+        s = path.split("/")
+        
+        for i in range(len(s)):
+
+            if s[i] == "" or s[i] == ".":
+               continue
+            if stack and s[i] == "..":
+              
+               stack.pop()
+            else:
+                continue
+            
+            stack.append(s[i])
+               
+        print(s)
+        print(stack)
+        return res + "/".join(stack)
+sol = Solution()
+ans = sol.simplifyPath("home")
+print(ans)
