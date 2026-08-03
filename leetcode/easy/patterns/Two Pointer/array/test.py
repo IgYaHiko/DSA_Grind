a720e55b6a0feb082830f017b798eae2ea8f3548
IgYaHiko <subhrokolay2@gmail.com>
2026-08-03 23:47:25 +0530

    test

--- leetcode/easy/patterns/Two Pointer/array/test.py
+++ leetcode/easy/patterns/Two Pointer/array/test.py
@@ -1,11 +1,3 @@
 s = "abccccdd"
 freq = {}
-res = 0
-for i in range(len(s)):
-    freq[s[i]] = freq.get(s[i],0) + 1
-    #if len(s) % 2 == 0:
-    print(freq[s[i]])
-print(res)
-       
-          
-          
+    
