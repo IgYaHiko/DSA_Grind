6082cd56b11a8d838ccd00ae3801dd19df6c2acc
IgYaHiko <subhrokolay2@gmail.com>
2026-08-12 23:52:56 +0530

    33. Search in Rotated Sorted Array

---
+++ leetcode/medium/patterns/binary-search/33-Search-in-Rotated-Sorted-Array.py
@@ -0,0 +1,23 @@
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        i = 0
+        j = len(nums) -1
+        while i<=j:
+            mid = (i+j)//2
+            if nums[mid] == target:
+               return mid
+            if nums[i] <= nums[mid]:
+                if nums[i] <= target < nums[mid]:
+                  j = mid - 1
+                else:
+                  i = mid + 1
+            elif nums[mid] <= nums[j]:
+                if nums[mid] < target <= nums[j]:
+                   i = mid + 1
+                else:
+                   j = mid - 1
+        return -1
+
+sol = Solution()
+ans = sol.search(nums = [4,5,6,7,0,1,2], target = 0)
+print(ans)                
