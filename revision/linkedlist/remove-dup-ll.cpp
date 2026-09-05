59d7c837d9b90b2a277ae149ebe4abd071de61f8
IgYaHiko <subhrokolay2@gmail.com>
2026-09-05 16:05:37 +0530

    remove-duplicate

---
+++ revision/linkedlist/remove-dup-ll.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include"LL/ll.h"
+using namespace std;
+class Solution {
+public:
+    My_linkedList::Node* removeDup(My_linkedList::Node* head) {
+        My_linkedList::Node* curr = head;
+        while(curr != nullptr && curr->next != nullptr) {
+            if(curr->data == curr->next->data) {
+                curr->next = curr->next->next;
+            } else {
+                curr = curr->next;
+            }
+        }
+    return head;
+    }
+};
+int main() {
+    Solution sol;
+    My_linkedList ll;
+    ll.insert(1);
+    ll.insert(2);
+    ll.insert(2);
+    ll.insert(2);
+    ll.insert(3);
+    ll.insert(5);
+
+    My_linkedList::Node* res = sol.removeDup(ll.head);
+
+    while (res != nullptr && res->next != nullptr)  
+    {
+        cout << res->data << " -> ";
+        res = res->next;
+    }
+    return 0;
+    
+}
